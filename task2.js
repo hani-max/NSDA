a5d3c1ca4e55a8efb5ad4dae9ef9fd819aab9ea3
hani-max <hananalimohamed2003@gmail.com>
2025-09-12 19:38:56 +0300

    Update task2.js

---
+++ task2.js
@@ -0,0 +1,22 @@
+const task1 = {
+  id: 1,
+  name: "Learn JavaScript",
+  completed: true,
+};
+
+const task2 = {
+  id: 2,
+  name: "Do homework",
+  completed: true,
+};
+
+const task3 = {
+  id: 3,
+  name: "takeing a walk",
+  completed: false,
+};
+
+const tasks = [task1, task2, task3];
+
+console.log("Task List:");
+console.log(tasks);
